ee7fdb21be33c6fc088c8c9cc79f2e33a0f05b07
JiaJi Yin <jjyin@bainainfo.com>
2013-10-22 20:33:59 +0800

    Initial Commit

---
+++ BoothBuddyDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BoothBuddyDemo
+//
+//  Created by Foster Yin on 10/22/13.
+//  Copyright (c) 2013 Foster Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
